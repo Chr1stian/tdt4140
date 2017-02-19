dc0ec7bc38c8b42c3aeea39b40dff88c28775523
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-02-19 20:15:20 +0100

    added button OnClick function

--- App/Prodo/app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/MainActivity.java
+++ App/Prodo/app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/MainActivity.java
@@ -2,12 +2,31 @@ package nogroupsprodoteam.ntnu.stud.httpwww.prodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txt_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt_name = (EditText) findViewById(R.id.txt_name);
+
+
+        Button btn_ok = (Button) findViewById(R.id.btn_ok);
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                txt_name.setText("Kult navn");
+            }
+        });
+
     }
+
+
+
 }
