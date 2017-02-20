e2c8be13f6bfca6994df64f0e81ed4ebffd44f1d
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-02-20 15:31:29 +0100

    added views for selection of nickname, course and lecture

--- App/Prodo/app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/MainActivity.java
+++ App/Prodo/app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/MainActivity.java
@@ -2,13 +2,17 @@ package nogroupsprodoteam.ntnu.stud.httpwww.prodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Selection;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText txt_name;
+    public final static String EXTRA_MESSAGE = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +25,18 @@ public class MainActivity extends AppCompatActivity {
         Button btn_ok = (Button) findViewById(R.id.btn_ok);
         btn_ok.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                txt_name.setText("Kult navn");
+                sendMessage();
             }
         });
 
+    }
+
+    public void sendMessage() {
+        Intent intent = new Intent(this, SelectionActivity.class);
+        EditText editText = (EditText) findViewById(R.id.txt_name);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 
