909776247c1b41a11fabb14fc3392a1db0bee71c
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-02-21 12:58:24 +0100

    added databaseconnection for application

---
+++ App/Prodo/app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/Database.java
@@ -0,0 +1,35 @@
+package nogroupsprodoteam.ntnu.stud.httpwww.prodo;
+
+/**
+ * Created by Christian on 21.02.2017.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Database {
+    private static String mysqlAddr = "jdbc:mysql://mysql.stud.ntnu.no:3306/prodoteam_db?allowMultiQueries=true";
+    private static String mysqlUser = "chrisnyv_demo";
+    private static String mysqlPass = "rM48DmzH";
+
+
+
+    public static String test(){
+        try{
+            Connection conn = DriverManager.getConnection(mysqlAddr, mysqlUser, mysqlPass);
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM bruker");
+
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next())
+                return rs.getString(2);
+            return null;
+        }
+        catch(SQLException e){
+            return e.toString();
+        }
+    }
+
+}
--- App/Prodo/app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/MainActivity.java
+++ App/Prodo/app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/MainActivity.java
@@ -1,16 +1,17 @@
 package nogroupsprodoteam.ntnu.stud.httpwww.prodo;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Selection;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
-import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Database db = new Database();
     EditText txt_name;
     public final static String EXTRA_MESSAGE = "";
 
@@ -21,11 +22,11 @@ public class MainActivity extends AppCompatActivity {
 
         txt_name = (EditText) findViewById(R.id.txt_name);
 
-
         Button btn_ok = (Button) findViewById(R.id.btn_ok);
         btn_ok.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                sendMessage();
+                //sendMessage();
+                txt_name.setText(db.test());
             }
         });
 
