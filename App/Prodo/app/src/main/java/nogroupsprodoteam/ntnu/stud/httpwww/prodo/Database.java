94db644fee127a5193c4b001ce55fa2e6bf0fc1c
Christian André Nyvoll <chrisnyv@stud.ntnu.no>
2017-02-22 19:12:18 +0100

    cleanup

--- App/Prodo/app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/Database.java
+++ App/Prodo/app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/Database.java
@@ -4,11 +4,14 @@ package nogroupsprodoteam.ntnu.stud.httpwww.prodo;
  * Created by Christian on 21.02.2017.
  */
 
+import android.os.StrictMode;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Database {
     private static String mysqlAddr = "jdbc:mysql://mysql.stud.ntnu.no:3306/prodoteam_db?allowMultiQueries=true";
@@ -18,6 +21,11 @@ public class Database {
 
 
     public static String test(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         try{
             Connection conn = DriverManager.getConnection(mysqlAddr, mysqlUser, mysqlPass);
             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM bruker");
@@ -29,6 +37,30 @@ public class Database {
         }
         catch(SQLException e){
             return e.toString();
+        }
+    }
+
+    public static ArrayList<String> courseCodes(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> course = new ArrayList<String>();
+        try{
+            Connection conn = DriverManager.getConnection(mysqlAddr, mysqlUser, mysqlPass);
+            PreparedStatement stmt = conn.prepareStatement("SELECT code FROM course");
+
+            ResultSet rs = stmt.executeQuery();
+            int i = 0;
+            while(rs.next()){
+                course.add(rs.getString(1));
+            }
+            return course;
+        }
+        catch(SQLException e){
+            System.out.println(e);
+            return null;
         }
     }
 
--- App/Prodo/app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/MainActivity.java
+++ App/Prodo/app/src/main/java/nogroupsprodoteam/ntnu/stud/httpwww/prodo/MainActivity.java
@@ -1,28 +1,15 @@
 package nogroupsprodoteam.ntnu.stud.httpwww.prodo;
 
-import android.database.SQLException;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telecom.Connection;
-import android.text.Selection;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
-import android.view.View;
-
-import com.mysql.jdbc.PreparedStatement;
-import com.mysql.jdbc.*;
-
-import java.sql.*;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Statement;
 
 
 public class MainActivity extends AppCompatActivity {
-    Database db = new Database();
     EditText txt_name;
     public final static String EXTRA_MESSAGE = "";
 
@@ -40,13 +27,8 @@ public class MainActivity extends AppCompatActivity {
         Button btn_ok = (Button) findViewById(R.id.btn_ok);
         btn_ok.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                try {
-                    Class.forName("com.mysql.jdbc.Driver");
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
+                //txt_name.setText(Database.test());
                 sendMessage();
-                //txt_name.setText(db.test());
             }
         });
 
